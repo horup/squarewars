a7e8052ec77c2a87eb82b0145aa45ed5d0c3d91a
= <horup@deepstacker.com>
2024-07-10 11:02:27 +0200

    wip

--- src/main.zig
+++ src/main.zig
@@ -7,14 +7,9 @@ const ray = @cImport({
 
 pub fn main() !void {
     try ray_main();
-    try old_main(); // remove this if you don't need it
-    try hints();
 }
 
 fn ray_main() !void {
-    // const monitor = ray.GetCurrentMonitor();
-    // const width = ray.GetMonitorWidth(monitor);
-    // const height = ray.GetMonitorHeight(monitor);
     const width = 800;
     const height = 450;
 
@@ -66,43 +61,4 @@ fn ray_main() !void {
             ray.DrawFPS(width - 100, 10);
         }
     }
-}
-
-// remove this function if you don't need it
-fn old_main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-fn hints() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n", .{});
-    try stdout.print("Here are some hints:\n", .{});
-    try stdout.print("Run `zig build --help` to see all the options\n", .{});
-    try stdout.print("Run `zig build -Doptimize=ReleaseSmall` for a small release build\n", .{});
-    try stdout.print("Run `zig build -Doptimize=ReleaseSmall -Dstrip=true` for a smaller release build, that strips symbols\n", .{});
-    try stdout.print("Run `zig build -Draylib-optimize=ReleaseFast` for a debug build of your application, that uses a fast release of raylib (if you are only debugging your code)\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
